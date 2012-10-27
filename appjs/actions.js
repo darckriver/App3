85a9a886292fb112ca9899ca99b75489d724f6b8
Luis Perez <luiggiz@hotmail.com>
2012-10-27 14:32:56 -0500

    comitt no se cual

--- appjs/actions.js
+++ appjs/actions.js
@@ -3,7 +3,7 @@ function deviceInformacion(){
 var s='Esto es otra prueba';
 		$('#devic table td').eq(1).text('android luis');
 		//$('#device table td').eq(3).text(device.cordova);
-		$('#devic table td').eq(5).text(window.device.name + ' Hola');
+		$('#devic table td').eq(5).text(device.name + ' Hola');
 		$('#devic table td').eq(7).text('Adios');
 		$('#devic table td').eq(9).text(s);	
 }
