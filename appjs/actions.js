092b716b1b41a322d90773637f9c50f83ac26a6f
Luis Perez <luiggiz@hotmail.com>
2012-10-27 11:46:51 -0500

    comit4

--- appjs/actions.js
+++ appjs/actions.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 function deviceInfo(){
 		$('#device table td').eq(1).text(device.name);
-		$('#device table td').eq(3).text(device.phonegap);
+		$('#device table td').eq(3).text(device.cordova);
 		$('#device table td').eq(5).text(device.plataform);
 		$('#device table td').eq(7).text(device.version);
 		$('#device table td').eq(9).text(device.uuid);	
