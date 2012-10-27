5fef829bb666567d0a3df84a1749a6a34ebd7480
Luis Perez <luiggiz@hotmail.com>
2012-10-27 12:38:17 -0500

    comit7

--- appjs/actions.js
+++ appjs/actions.js
@@ -1,10 +1,10 @@
 // JavaScript Document
 function deviceInfo(){
 		$('#device table td').eq(1).text('android luis');
-		$('#device table td').eq(3).text(device.cordova);
-		$('#device table td').eq(5).text(device.plataform);
-		$('#device table td').eq(7).text(device.version);
-		$('#device table td').eq(9).text(device.uuid);	
+		//$('#device table td').eq(3).text(device.cordova);
+		$('#device table td').eq(5).text(device.name);
+		//$('#device table td').eq(7).text(device.version);
+		//$('#device table td').eq(9).text(device.uuid);	
 }
 
 $(document).ready(function(){    
