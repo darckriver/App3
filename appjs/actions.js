a15b9223b983b5260f59f08691b6d2d24d20c9c6
Luis Perez <luiggiz@hotmail.com>
2012-10-27 12:26:44 -0500

    comit5

--- appjs/actions.js
+++ appjs/actions.js
@@ -7,9 +7,6 @@ function deviceInfo(){
 		$('#device table td').eq(9).text(device.uuid);	
 }
 
-$(document).ready(function(){
-    
-	document.addEventListener("deviceready",function(){
-		deviceInfo();
-	}, false);
+$(document).ready(function(){    
+	document.addEventListener("deviceready",deviceInfo(), false);
 });
