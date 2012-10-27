de720dc71fb7195ad326c27745d1dd8817992927
Luis Perez <luiggiz@hotmail.com>
2012-10-27 12:53:59 -0500

    comit9

--- appjs/actions.js
+++ appjs/actions.js
@@ -1,10 +1,11 @@
 // JavaScript Document
 function deviceInfo(){
+var s='Esto es otra prueba'
 		$('#device table td').eq(1).text('android luis');
 		//$('#device table td').eq(3).text(device.cordova);
 		$('#device table td').eq(5).text(windows.device.name + ' Hola');
-		//$('#device table td').eq(7).text(device.version);
-		//$('#device table td').eq(9).text(device.uuid);	
+		$('#device table td').eq(7).text('Adios');
+		$('#device table td').eq(9).text(s);	
 }
 
 $(document).ready(function(){    
